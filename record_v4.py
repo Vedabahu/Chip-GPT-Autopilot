8acac01b58a3e6c45f6e66315ec5092206cc8f08
Vedabahu <79869936+Vedabahu@users.noreply.github.com>
2025-03-09 11:16:49 +0530

    updated to invert image

--- record_v4.py
+++ record_v4.py
@@ -1,9 +1,11 @@
 from picamera2.encoders import H264Encoder
 from picamera2 import Picamera2, Preview
+from libcamera import Transform
 import time
 
 picam2 = Picamera2()
-video_config = picam2.create_video_configuration()
+# video_config = picam2.create_video_configuration()
+video_config = picam2.create_preview_configuration(main={"size": (640, 480)}, transform=Transform(vflip=True))
 picam2.configure(video_config)
 # picam2.start_preview(Preview.QT)
 
